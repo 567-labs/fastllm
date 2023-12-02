1af957722966ad6a28913df6a4265af3dc35d009
Jason Liu <jason@jxnl.co>
2023-12-01 23:40:00 -0500

    issues

--- applications/wikipedia/download.py
+++ applications/wikipedia/download.py
@@ -1,31 +1,22 @@
-""" 
-This script is to test downloading a model from the HuggingFace Hub 
-and persisting it to a volume.
-"""
-from modal import Image, Stub, Volume, Secret
+from modal import Image, Stub, Volume
 
-MODEL_ID = "BAAI/bge-base-en-v1.5"
-BATCH_SIZE = 32
+stub = Stub("wikipedia")
+
+vol = Volume.persisted("wikipedia")
 
 
-def download_model():
-    from huggingface_hub import hf_hub_download
-
-    hf_hub_download(repo_id="BAAI", filename="bge-base-en-v1.5")
-
-
-stub = Stub("embeddings")
-
-vol = Volume.persisted("embeddings")
-
-
-def download_data():
+@stub.function(
+    image=Image.debian_slim().pip_install("tqdm", "datasets"),
+    volumes={"/data": vol},
+    timeout=300,
+)
+def embed_dataset():
+    # >>> modal run download.py::embed_dataset
     from datasets import load_dataset, load_from_disk
 
-    # "20210301.en" use a smaller dataset for testing
     WIKI, SET = (
         "wikipedia",
-        "20220301.ab",
+        "20220301.frr",  # "20210301.en" but its too big for testings
     )
     PATH = f"/data/{WIKI}-{SET}"
 
@@ -40,15 +31,5 @@ def download_data():
     dataset = load_dataset(WIKI, SET)
     dataset.save_to_disk(PATH)
     vol.commit()
-    return dataset
 
-
-@stub.function(
-    image=Image.debian_slim().pip_install("tqdm", "datasets", "apache-beam"),
-    volumes={"/data": vol},
-    timeout=300 * 6,
-)
-def embed_dataset():
-    # >>> modal run download.py::embed_dataset
-    dataset = download_data()
     print(f"{len(dataset)=}")
