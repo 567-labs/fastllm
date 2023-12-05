cea1e77e47aaaec25ded117c2e77abec27113b8c
Jason Liu <jason@jxnl.co>
2023-12-05 11:38:11 -0500

    ruff and update batch

--- applications/wikipedia/main.py
+++ applications/wikipedia/main.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 import json
 import subprocess
 from pathlib import Path
@@ -5,10 +6,10 @@ from modal import Image, Stub, Volume, gpu, method, Secret
 import threading
 import time
 
-N_GPU = 2
+N_GPU = 6
 GPU_CONFIG = gpu.A10G()
 MODEL_ID = "BAAI/bge-base-en-v1.5"
-BATCH_SIZE = 32
+BATCH_SIZE = 256
 ENABLE_WANDB = True
 WANDB_PROJECT = "MODAL_EMBEDDING_RUN"
 WANDB_GROUP = "RUN-1"
@@ -29,6 +30,8 @@ LAUNCH_FLAGS = [
     MODEL_ID,
     "--port",
     "8000",
+    "--max-client-batch-siz",
+    str(BATCH_SIZE),
 ]
 
 
@@ -66,8 +69,7 @@ tei_image = (
     )
     .dockerfile_commands("ENTRYPOINT []")
     .run_function(download_model, gpu=GPU_CONFIG)
-    .pip_install("httpx","wandb")
-    
+    .pip_install("httpx", "wandb")
 )
 
 
@@ -92,16 +94,26 @@ def generate_batches(xs, batch_size=50):
     if batch:
         yield batch
 
+
 def get_gpu_utilization():
     try:
-        sp = subprocess.Popen(['nvidia-smi', '--query-gpu=utilization.gpu', '--format=csv,noheader,nounits'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        sp = subprocess.Popen(
+            [
+                "nvidia-smi",
+                "--query-gpu=utilization.gpu",
+                "--format=csv,noheader,nounits",
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
         out_str = sp.communicate()
-        out_list = out_str[0].decode('utf-8').split('\n')
+        out_list = out_str[0].decode("utf-8").split("\n")
         out_list = [x for x in out_list if x]
         return [round(float(x) / 100, 3) for x in out_list]
     except Exception as e:
         print("Exception: ", e)
         return []
+
 
 @stub.cls(
     gpu=GPU_CONFIG,
@@ -110,36 +122,36 @@ def get_gpu_utilization():
     concurrency_limit=N_GPU,
     # Allow each container to process up to 10 batches at once.
     allow_concurrent_inputs=100,
-    secret=Secret.from_name("wandb")
+    secret=Secret.from_name("wandb"),
 )
 class TextEmbeddingsInference:
     def __enter__(self):
         # If the process is running for a long time, the client does not seem to close the connections, results in a pool timeout
         from httpx import AsyncClient
         import wandb
+
         self.keep_running = True
         self.process = spawn_server()
         self.client = AsyncClient(base_url="http://127.0.0.1:8000")
 
         if ENABLE_WANDB:
-            wandb.init(
-                project=WANDB_PROJECT, group=WANDB_GROUP, reinit=True
-            )
-       
+            wandb.init(project=WANDB_PROJECT, group=WANDB_GROUP, reinit=True)
+
         self.gpu_utilization_figures = []
+
         def record_gpu_utilization():
             while self.keep_running:
-                curr_utilization= get_gpu_utilization()
+                curr_utilization = get_gpu_utilization()
                 self.gpu_utilization_figures.append(curr_utilization[0])
                 time.sleep(1)
 
         self.gpu_utilization_thread = threading.Thread(target=record_gpu_utilization)
         self.gpu_utilization_thread.start()
 
-
     def __exit__(self, _exc_type, _exc_value, _traceback):
         import wandb
-        self.keep_running=False
+
+        self.keep_running = False
         self.gpu_utilization_thread.join()
         wandb.finish()
         self.process.terminate()
@@ -150,24 +162,23 @@ class TextEmbeddingsInference:
         n_chars = sum(map(len, texts))
         res = await self.client.post("/embed", json={"inputs": texts})
         embeddings = res.json()
-        end = time.perf_counter();
-        total_time = int(end-start)
+        end = time.perf_counter()
+        total_time = int(end - start)
         snapshot = self.gpu_utilization_figures[-total_time:]
         if not snapshot:
             snapshot = [get_gpu_utilization()[0]]
-        gpu_utilization = sum(snapshot)/len(snapshot)
-        return list(zip(texts,embeddings)),n_chars,gpu_utilization
+        gpu_utilization = sum(snapshot) / len(snapshot)
+        return list(zip(texts, embeddings)), n_chars, gpu_utilization
 
 
 @stub.function(
-    image=Image.debian_slim().pip_install("datasets","pyarrow"),
+    image=Image.debian_slim().pip_install("datasets", "pyarrow"),
     volumes={cache_dir: volume},
     timeout=5000,
     secret=Secret.from_name("huggingface-credentials"),
-
 )
-def embed_dataset(down_scale: float = 0.005,upload_dataset_to_hf=False):
-    from datasets import load_from_disk,load_dataset
+def embed_dataset(down_scale: float = 0.005, upload_dataset_to_hf=False):
+    from datasets import load_from_disk, load_dataset
     import pyarrow as pa
     import pyarrow.parquet as pq
     import os
@@ -198,9 +209,7 @@ def embed_dataset(down_scale: float = 0.005,upload_dataset_to_hf=False):
     print(f"Working with {sample_size} rows")
 
     text_chunks = generate_chunks_from_dataset(subset, chunk_size=400)
-    batches = generate_batches(
-        text_chunks, batch_size=32
-    ) # 32 is the max batch size of the model so we increase by 10x hehe
+    batches = generate_batches(text_chunks, batch_size=BATCH_SIZE)
 
     start = time.perf_counter()
     materialized_batchs = list(batches)
@@ -212,23 +221,33 @@ def embed_dataset(down_scale: float = 0.005,upload_dataset_to_hf=False):
     counter = 0
     combined_embedding_and_text = []
     gpu_utilization_snapshot = []
-    for embedding_and_text,n_chars,gpu_utilization in model.embed.map(materialized_batchs, order_outputs=False):
+    for embedding_and_text, n_chars, gpu_utilization in model.embed.map(
+        materialized_batchs, order_outputs=False
+    ):
         counter += n_chars
         combined_embedding_and_text.extend(embedding_and_text)
         gpu_utilization_snapshot.append(gpu_utilization)
     end = time.perf_counter()
 
     if upload_dataset_to_hf:
+        start = time.perf_counter()
         texts = [i[0] for i in combined_embedding_and_text]
         embeddings = [i[1] for i in combined_embedding_and_text]
         text_array = pa.array(texts)
         embedding_array = pa.array(embeddings)
-        table = pa.Table.from_arrays([text_array, embedding_array], names=["text", "embedding"])
+        table = pa.Table.from_arrays(
+            [text_array, embedding_array], names=["text", "embedding"]
+        )
 
         pq.write_table(table, "wiki-embeddings.parquet")
         dataset = load_dataset("parquet", data_files="wiki-embeddings.parquet")
-        dataset.push_to_hub("ivanleomk/wikipedia-embeddings-trial",token=os.environ["HUGGINGFACE_TOKEN"])
-    
+        dataset.push_to_hub(
+            "ivanleomk/wikipedia-embeddings-trial",
+            token=os.environ["HUGGINGFACE_TOKEN"],
+        )
+        end = time.perf_counter()
+        print(f"Uploaded dataset in {end-start:.2f} seconds")
+
     duration = end - start
     characters_per_second = int(counter / duration)
     extrapolated_duration = int(duration / down_scale)
@@ -244,7 +263,8 @@ def embed_dataset(down_scale: float = 0.005,upload_dataset_to_hf=False):
         "extrapolated_duration": extrapolated_duration,
         "extrapolated_duration_fmt": extrapolated_duration_fmt,
         "extrapolated_duration_tps_fmt": extrapolated_duration_tps_fmt,
-        "estimated_gpu_load": sum(gpu_utilization_snapshot)/len(gpu_utilization_snapshot)
+        "estimated_gpu_load": sum(gpu_utilization_snapshot)
+        / len(gpu_utilization_snapshot),
     }
 
 
