4f53aa82579cf9cae51abaecfaf758a5f0286aee
Justin Liang <justinliang1020@gmail.com>
2023-12-20 11:06:03 -0500

    rename modal-main and change caching

--- applications/sentence-transformers-ft/modal-main.py
+++ applications/sentence-transformers-ft/modal-main.py
@@ -4,17 +4,16 @@ import modal
 import pathlib
 from finetune import finetune
 
+MODEL_ID = "BAAI/bge-small-en-v1.5"
 
 # Modal constants
 VOL_MOUNT_PATH = pathlib.Path("/vol")
 GPU_CONFIG = "a10g"
+USE_CACHED_IMAGE = True  # enable this to download the dataset and base model into the image for faster repeated runs
 
 
-# Functions for Modal Image build steps (cache the model and dataset)
-# NOTE: this can be removed for simplicity, this is currently here for faster devloop times when repeatedly running the function
 def download_model():
-    model_id = "BAAI/bge-small-en-v1.5"
-    SentenceTransformer(model_id)
+    SentenceTransformer(MODEL_ID)
 
 
 def download_dataset():
@@ -24,18 +23,18 @@ def download_dataset():
 
 # Modal resources
 # TODO: uncomment this to make the volume persisted
+# TODO: for final code just make it persisted, right now leave it commented for dev purposes
 # volume = modal.Volume.persisted(
 #     f"sentence-transformers-ft-{int(datetime.now().timestamp())}"
 # )
 # non-persistent volume for dev purposes
 volume = modal.Volume.new()
 stub = modal.Stub("finetune-embeddings")
-image = (
-    modal.Image.debian_slim()
-    .pip_install("sentence-transformers", "torch", "datasets")
-    .run_function(download_model)
-    .run_function(download_dataset)
+image = modal.Image.debian_slim().pip_install(
+    "sentence-transformers", "torch", "datasets"
 )
+if USE_CACHED_IMAGE:
+    image = image.run_function(download_model).run_function(download_dataset)
 
 
 @stub.function(
@@ -46,12 +45,7 @@ image = (
     _allow_background_volume_commits=True,
 )
 def finetune_modal():
-    model_id = "BAAI/bge-small-en-v1.5"
-    dataset_fraction = 300
-
-    finetune(
-        model_id=model_id, dataset_fraction=dataset_fraction, save_path=VOL_MOUNT_PATH
-    )
+    finetune(model_id=MODEL_ID, dataset_fraction=300, save_path=VOL_MOUNT_PATH)
 
 
 # run on modal with `modal run main.py`
