711e5986d424e54a8f3630f8a7bd3362724b8a64
Justin Liang <justinliang1020@gmail.com>
2023-12-25 16:34:05 -0500

    modal optuna big-ish run

--- applications/sentence-transformers-ft/modal-optuna.py
+++ applications/sentence-transformers-ft/modal-optuna.py
@@ -8,20 +8,14 @@ from datetime import datetime
 import torch
 
 
-MODEL_ID = "BAAI/bge-small-en-v1.5"
-
 # Modal constants
 GPU_CONFIG = "a10g"
-N_GPU = 4
-N_TRIALS = 3  # Number of trials PER gpu. Total trials = N_GPU * N_TRIALS
+N_GPU = 10
+N_TRIALS = 10  # Number of trials PER gpu. Total trials = N_GPU * N_TRIALS
 USE_CACHED_IMAGE = True  # enable this to download the dataset and base model into the image for faster repeated runs
 
 
-# Functions for Modal Image build steps (cache the model and dataset)
-def download_model():
-    SentenceTransformer(MODEL_ID)
-
-
+# Functions for Modal Image build steps (cache the dataset)
 def download_dataset():
     dataset_id = "quora"
     load_dataset(dataset_id, split="train")
@@ -30,27 +24,23 @@ def download_dataset():
 # Modal resources
 stub = modal.Stub("finetune-embeddings-optuna")
 image = modal.Image.debian_slim().pip_install(
-    "sentence-transformers", "torch", "datasets", "optuna"
+    "sentence-transformers", "torch", "datasets", "optuna", "pandas"
 )
 if USE_CACHED_IMAGE:
-    image = image.run_function(download_model).run_function(download_dataset)
+    image = image.run_function(download_dataset)
 
 
-# TODO: attach this to stub?
-nfs_volume = modal.NetworkFileSystem.new()
+stub.nfs_volume = modal.NetworkFileSystem.new()
 JOURNAL_PATH = "/root/cache/journal.log"
 STUDY_NAME = "sentence-transformers-ft study"
 VOL_MOUNT_PATH = pathlib.Path("/vol")
 
-# TODO: uncomment this to make the volume persisted
 volume = modal.Volume.persisted(
     f"sentence-transformers-ft-optuna-{int(datetime.now().timestamp())}"
 )
-# non-persistent volume for dev purposes
-# volume = modal.Volume.new()
 
 
-@stub.function(image=image, network_file_systems={"/root/cache": nfs_volume})
+@stub.function(image=image, network_file_systems={"/root/cache": stub.nfs_volume})
 def initialize_optuna():
     storage = optuna.storages.JournalStorage(
         optuna.storages.JournalFileStorage(JOURNAL_PATH)
@@ -61,33 +51,25 @@ def initialize_optuna():
 
 
 def objective(trial: optuna.Trial):
-    # TODO: set more interesting hyperparameter searches
     # 1/3 to double embedding count
-    # TODO: Disable log
-    dense_out_features = trial.suggest_int(
-        "dense_out_features", 200, 800
-    )  # TODO: Need to also somehow suggest "None" for running without dense layer, or this could just be manually set by user in the script
-    # NOTE: dense_out_features seem to have no effect
-    activation_function_str = trial.suggest_categorical(
-        "activation", ["Tanh", "ReLU", "Sigmoid"]
-    )
-    # NOTE: sigmoid seems to be terrible
-    activation_function = getattr(torch.nn, activation_function_str)()
-    epochs = trial.suggest_int("epochs", 7, 12, log=True)
 
+    # Optuna Hyperparameters
+    dense_out_features = trial.suggest_int("dense_out_features", 100, 700)
+    activation_function_str = trial.suggest_categorical("activation", ["Tanh", "ReLU"])
+    activation_function = getattr(torch.nn, activation_function_str)()
+    epochs = trial.suggest_int("epochs", 7, 12)
     scheduler = trial.suggest_categorical(
         "scheduler", ["warmupconstant", "warmuplinear", "warmupcosine"]
     )
-
     model_id = trial.suggest_categorical(
         "model_id",
         [
             "BAAI/bge-small-en-v1.5",
-            "BAAI/bge-base-en-v1.5",
+            # "BAAI/bge-base-en-v1.5",
+            # "BAAI/bge-large-en-v1.5",
             "thenlper/gte-small",
             "intfloat/e5-small-v2",
             "sentence-transformers/all-MiniLM-L12-v2",
-            "sentence-transformers/all-mpnet-base-v2",
         ],
     )
 
@@ -98,7 +80,7 @@ def objective(trial: optuna.Trial):
         save_path=VOL_MOUNT_PATH / f"trial-{trial.number}",
         dense_out_features=dense_out_features,
         epochs=epochs,
-        dataset_fraction=1000,
+        dataset_fraction=5,
         activation_function=activation_function,
         scheduler=scheduler,
     )
@@ -111,7 +93,7 @@ def objective(trial: optuna.Trial):
     timeout=15000,
     volumes={VOL_MOUNT_PATH: volume},
     _allow_background_volume_commits=True,
-    network_file_systems={"/root/cache": nfs_volume},
+    network_file_systems={"/root/cache": stub.nfs_volume},
     concurrency_limit=N_GPU,
 )
 def run_optuna(i: int):
@@ -128,7 +110,12 @@ def run_optuna(i: int):
     return i
 
 
-@stub.function(image=image, network_file_systems={"/root/cache": nfs_volume})
+@stub.function(
+    image=image,
+    network_file_systems={"/root/cache": stub.nfs_volume},
+    volumes={VOL_MOUNT_PATH: volume},
+    _allow_background_volume_commits=True,
+)
 def conclude_optuna():
     # TODO: prints trials to keep logs in modal
     storage = optuna.storages.JournalStorage(
@@ -137,9 +124,14 @@ def conclude_optuna():
     study = optuna.load_study(study_name=STUDY_NAME, storage=storage)
 
     trials = study.get_trials()
-    print(trials)
-    # TODO: get best trial and return that model?
-    # find the highest trial number, get that directory, load model from that directory, return model
+    print("### All Trials ###\n", trials)
+
+    df = study.trials_dataframe()
+    df.to_csv(VOL_MOUNT_PATH / "trials.csv")
+
+    best_trial = study.best_trial
+    print("### Best Trial ID:", best_trial._trial_id)
+    # TODO: find the highest trial number, get that directory, load model from that directory, return model
     return trials
 
 
@@ -147,8 +139,6 @@ def conclude_optuna():
 def main():
     initialize_optuna.remote()
     # Run Optuna optimization
-    for r in run_optuna.map(range(1, N_GPU + 1)):
-        # TODO: fix this
-        i = r
+    for i in run_optuna.map(range(1, N_GPU + 1)):
         print(f"Finished training on gpu container {i}.")
     print(conclude_optuna.remote())
