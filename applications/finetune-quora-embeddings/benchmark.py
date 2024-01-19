16d3ea31eee789d5472141742e400944ea0d3a54
Ivan <ivanleomk@gmail.com>
2024-01-19 22:39:28 +0800

    Updated README and the benchmarking script to do a single embedding job and then read off an embedding layer

--- applications/finetune-quora-embeddings/benchmark.py
+++ applications/finetune-quora-embeddings/benchmark.py
@@ -7,9 +7,7 @@ GPU_CONFIG = gpu.A100()
 
 
 # Dataset Configuration
-DATASET_NAME = "quora"
-DATASET_CONFIG = "default"
-DATASET_SPLIT = "train"
+DATASET_NAME = "567-labs/cleaned-quora-dataset-train-test-split"
 DATASET_DIR = "/data"
 DATASET_VOLUME = Volume.persisted("datasets")
 
@@ -28,7 +26,7 @@ image = Image.debian_slim().pip_install(
 
 @stub.function(image=image, volumes={DATASET_DIR: DATASET_VOLUME})
 def generate_dataset_split():
-    from datasets import load_dataset, DatasetDict, load_from_disk
+    from datasets import load_dataset, load_from_disk
 
     dataset_path = f"{DATASET_DIR}/{DATASET_NAME}"
 
@@ -36,20 +34,9 @@ def generate_dataset_split():
         dataset = load_from_disk(f"{DATASET_DIR}/{DATASET_NAME}")
         return
 
-    dataset = load_dataset(
-        DATASET_NAME, DATASET_CONFIG, split=DATASET_SPLIT, num_proc=6
-    )
+    dataset = load_dataset(DATASET_NAME)
 
-    train_test_split = dataset.train_test_split(test_size=0.3, seed=42)
-    train = train_test_split["train"]
-    test_val_split = train_test_split["test"].train_test_split(test_size=0.5, seed=42)
-    test = test_val_split["train"]
-    val = test_val_split["test"]
-
-    new_ds = DatasetDict({"train": train, "test": test, "val": val})
-
-    new_ds.save_to_disk(dataset_path)
-
+    dataset.save_to_disk(dataset_path)
     DATASET_VOLUME.commit()
 
 
@@ -77,26 +64,54 @@ def benchmark_mteb_model(model_name):
     from sklearn.metrics import roc_auc_score
     import time
     import numpy as np
+    import torch.nn as nn
+    import torch
 
     dataset = load_from_disk(f"{DATASET_DIR}/{DATASET_NAME}")
-    val_dataset = dataset["val"]
+    test_dataset = dataset["test"]
 
     model = SentenceTransformer(model_name)
-    start = time.time()
 
-    # Process texts in batches
-    texts1 = [row["questions"]["text"][0] for row in val_dataset]
-    texts2 = [row["questions"]["text"][1] for row in val_dataset]
-    embeddings1 = model.encode(texts1, convert_to_tensor=True)
-    embeddings2 = model.encode(texts2, convert_to_tensor=True)
-    cosine_scores = util.cos_sim(embeddings1, embeddings2)
-    optimized_predictions = np.diag(cosine_scores.cpu()).tolist()
-    optimized_labels = [1 if row["is_duplicate"] else 0 for row in val_dataset]
+    sentences = []
+    sentences_id_to_embedding_mapping = {}
+    t1 = []
+    t2 = []
+    labels = []
+    for row in test_dataset:
+        s1, s2 = row["questions"]["text"]
+        id_1, id_2 = row["questions"]["id"]
 
-    print(f"Generated calculations in {time.time()-start}s")
+        if id_1 not in sentences_id_to_embedding_mapping:
+            sentences_id_to_embedding_mapping[id_1] = len(
+                sentences_id_to_embedding_mapping
+            )
+            sentences.append(s1)
 
-    # Calculate AUC score
-    return roc_auc_score(optimized_labels, optimized_predictions)
+        if id_2 not in sentences_id_to_embedding_mapping:
+            sentences_id_to_embedding_mapping[id_2] = len(
+                sentences_id_to_embedding_mapping
+            )
+            sentences.append(s2)
+
+        t1.append(sentences_id_to_embedding_mapping[id_1])
+        t2.append(sentences_id_to_embedding_mapping[id_2])
+        labels.append(1 if row["is_duplicate"] else 0)
+
+    embeddings = model.encode(sentences)
+    embeddings_tensor = torch.tensor(embeddings).to("cuda")
+
+    # Create an embedding layer with pre-trained weights
+    embedding_layer = nn.Embedding.from_pretrained(embeddings_tensor)
+
+    t1_tensor = torch.as_tensor(t1, device="cuda")
+    e1 = embedding_layer(t1_tensor)
+
+    t2_tensor = torch.as_tensor(t2, device="cuda")
+    e2 = embedding_layer(t2_tensor)
+
+    cosine_scores = util.cos_sim(e1, e2)
+    predictions = np.diag(cosine_scores.cpu()).tolist()
+    return roc_auc_score(labels, predictions)
 
 
 @stub.function(
@@ -104,6 +119,7 @@ def benchmark_mteb_model(model_name):
     volumes={DATASET_DIR: DATASET_VOLUME},
     secret=Secret.from_name("openai"),
     timeout=86400,
+    gpu=GPU_CONFIG,
 )
 async def benchmark_openai():
     from datasets import load_from_disk
@@ -112,45 +128,77 @@ async def benchmark_openai():
     import asyncio
     import time
     from tqdm import tqdm
-    from openai import OpenAI
+    from openai import AsyncOpenAI
+    import numpy as np
+    import torch
+    import torch.nn as nn
 
     dataset = load_from_disk(f"{DATASET_DIR}/{DATASET_NAME}")
-    val_dataset = dataset["val"]
+    test_dataset = dataset["test"]
 
-    client = OpenAI()
+    client = AsyncOpenAI()
 
-    sem = asyncio.Semaphore(64)
-    predictions = []
-    actual_label = []
+    sem = asyncio.Semaphore(20)
+    sentences = []
+    sentences_id_to_embedding_mapping = {}
+    t1 = []
+    t2 = []
+    labels = []
+    for row in test_dataset:
+        s1, s2 = row["questions"]["text"]
+        id_1, id_2 = row["questions"]["id"]
 
-    tqdm_monitoring_bar = tqdm(total=len(val_dataset))
+        if id_1 not in sentences_id_to_embedding_mapping:
+            sentences_id_to_embedding_mapping[id_1] = len(
+                sentences_id_to_embedding_mapping
+            )
+            sentences.append(s1)
 
-    async def embed_text(row, progress_bar: tqdm):
+        if id_2 not in sentences_id_to_embedding_mapping:
+            sentences_id_to_embedding_mapping[id_2] = len(
+                sentences_id_to_embedding_mapping
+            )
+            sentences.append(s2)
+
+        t1.append(sentences_id_to_embedding_mapping[id_1])
+        t2.append(sentences_id_to_embedding_mapping[id_2])
+        labels.append(1 if row["is_duplicate"] else 0)
+
+    batch_size = 16
+    print(f"Extracted {len(sentences)} unique sentences")
+    tqdm_monitoring_bar = tqdm(total=len(sentences))
+
+    async def embed_text(texts, progress_bar: tqdm):
         async with sem:
-            response = client.embeddings.create(
-                input=row["questions"]["text"], model="text-embedding-ada-002"
-            ).data
-            e1 = response[0].embedding
-            e2 = response[1].embedding
-            cosine_scores = util.cos_sim(e1, e2)
-            progress_bar.update(1)
-            return cosine_scores.item(), 1 if row["is_duplicate"] else 0
+            response = await client.embeddings.create(
+                input=texts, model="text-embedding-ada-002"
+            )
+            progress_bar.update(len(texts))
+            return [item.embedding for item in response.data]
 
     start = time.time()
-    res = await asyncio.gather(
-        *[embed_text(row, tqdm_monitoring_bar) for row in val_dataset]
-    )
+    coros = [
+        embed_text(sentences[start : start + batch_size], tqdm_monitoring_bar)
+        for start in range(0, len(sentences), batch_size)
+    ]
+    res = await asyncio.gather(*coros)
     tqdm_monitoring_bar.close()
     end = time.time()
-    print(f"Processed {len(val_dataset)} embeddings in {end-start}s")
 
-    predictions = [i[0] for i in res]
-    actual_label = [i[1] for i in res]
+    flattened_res = [item for sublist in res for item in sublist]
+    print(f"Processed {len(flattened_res)} embeddings in {end-start}s")
+    embeddings_tensor = torch.tensor(flattened_res).to("cuda")
+    embedding_layer = nn.Embedding.from_pretrained(embeddings_tensor)
 
-    # Calculate AUC score
-    auc = roc_auc_score(actual_label, predictions)
+    t1_tensor = torch.as_tensor(t1, device="cuda")
+    e1 = embedding_layer(t1_tensor)
 
-    return auc
+    t2_tensor = torch.as_tensor(t2, device="cuda")
+    e2 = embedding_layer(t2_tensor)
+
+    cosine_scores = util.cos_sim(e1, e2)
+    predictions = np.diag(cosine_scores.cpu()).tolist()
+    return roc_auc_score(labels, predictions)
 
 
 @stub.function(
@@ -158,6 +206,7 @@ async def benchmark_openai():
     volumes={DATASET_DIR: DATASET_VOLUME},
     secret=Secret.from_name("cohere"),
     timeout=86400,
+    gpu=GPU_CONFIG,
 )
 async def benchmark_cohere_roc():
     from cohere import Client
@@ -167,43 +216,77 @@ async def benchmark_cohere_roc():
     import asyncio
     import time
     from tqdm import tqdm
+    import numpy as np
+    import torch
+    import torch.nn as nn
 
     dataset = load_from_disk(f"{DATASET_DIR}/{DATASET_NAME}")
-    val_dataset = dataset["val"]
+    test_dataset = dataset["test"]
     co = Client(os.environ["COHERE_API_KEY"])
     sem = asyncio.Semaphore(64)
-    predictions = []
-    actual_label = []
+    sentences = []
+    sentences_id_to_embedding_mapping = {}
+    t1 = []
+    t2 = []
+    labels = []
+    for row in test_dataset:
+        s1, s2 = row["questions"]["text"]
+        id_1, id_2 = row["questions"]["id"]
 
-    tqdm_monitoring_bar = tqdm(total=len(val_dataset))
+        if id_1 not in sentences_id_to_embedding_mapping:
+            sentences_id_to_embedding_mapping[id_1] = len(
+                sentences_id_to_embedding_mapping
+            )
+            sentences.append(s1)
 
-    async def embed_text(row, progress_bar: tqdm):
+        if id_2 not in sentences_id_to_embedding_mapping:
+            sentences_id_to_embedding_mapping[id_2] = len(
+                sentences_id_to_embedding_mapping
+            )
+            sentences.append(s2)
+
+        t1.append(sentences_id_to_embedding_mapping[id_1])
+        t2.append(sentences_id_to_embedding_mapping[id_2])
+        labels.append(1 if row["is_duplicate"] else 0)
+
+    batch_size = 96
+    print(f"Extracted {len(sentences)} unique sentences")
+    tqdm_monitoring_bar = tqdm(total=len(sentences))
+
+    async def embed_text(texts, progress_bar: tqdm):
         async with sem:
             response = co.embed(
-                texts=row["questions"]["text"],
+                texts=texts,
                 model="embed-multilingual-v3.0",
                 input_type="clustering",
             )
-            e1, e2 = response.embeddings
-            cosine_scores = util.cos_sim(e1, e2)
-            progress_bar.update(1)
-            return cosine_scores.item(), 1 if row["is_duplicate"] else 0
+            progress_bar.update(len(texts))
+            return response.embeddings
 
     start = time.time()
-    res = await asyncio.gather(
-        *[embed_text(row, tqdm_monitoring_bar) for row in val_dataset]
-    )
+    coros = [
+        embed_text(sentences[start : start + batch_size], tqdm_monitoring_bar)
+        for start in range(0, len(sentences), batch_size)
+    ]
+    res = await asyncio.gather(*coros)
     tqdm_monitoring_bar.close()
     end = time.time()
-    print(f"Processed {len(val_dataset)} embeddings in {end-start}s")
+    print(f"Generated embeddings in {end-start}s")
 
-    predictions = [i[0] for i in res]
-    actual_label = [i[1] for i in res]
+    # Flatten the list of lists and convert to tensor of floats
+    flattened_res = [item for sublist in res for item in sublist]
+    embeddings_tensor = torch.tensor(flattened_res).to("cuda")
+    embedding_layer = nn.Embedding.from_pretrained(embeddings_tensor)
 
-    # Calculate AUC score
-    auc = roc_auc_score(actual_label, predictions)
+    t1_tensor = torch.as_tensor(t1, device="cuda")
+    e1 = embedding_layer(t1_tensor)
 
-    return auc
+    t2_tensor = torch.as_tensor(t2, device="cuda")
+    e2 = embedding_layer(t2_tensor)
+
+    cosine_scores = util.cos_sim(e1, e2)
+    predictions = np.diag(cosine_scores.cpu()).tolist()
+    return roc_auc_score(labels, predictions)
 
 
 @stub.local_entrypoint()
@@ -214,24 +297,24 @@ def main():
 
     res = {}
     res["text-embeddings-ada-v2"] = benchmark_openai.remote()
-    # res["embed-multilingual-v3.0"] = benchmark_cohere_roc.remote()
-    # models = [
-    #     "llmrails/ember-v1",
-    #     "BAAI/bge-base-en-v1.5",
-    #     "thenlper/gte-large",
-    #     "infgrad/stella-base-en-v2",
-    #     "sentence-transformers/gtr-t5-large",
-    #     "567-labs/bge-base-en-v1.5-ft-quora-0.9",
-    #     "567-labs/bge-base-en-v1.5-ft-quora-0.7",
-    #     "567-labs/bge-base-en-v1.5-ft-quora-0.5",
-    #     "567-labs/bge-base-en-v1.5-ft-quora-0.3",
-    #     "567-labs/bge-base-en-v1.5-ft-quora-0.1",
-    # ]
+    res["embed-multilingual-v3.0"] = benchmark_cohere_roc.remote()
+    models = [
+        "llmrails/ember-v1",
+        "BAAI/bge-base-en-v1.5",
+        "thenlper/gte-large",
+        "infgrad/stella-base-en-v2",
+        "sentence-transformers/gtr-t5-large",
+        # "567-labs/bge-base-en-v1.5-ft-quora-0.9",
+        # "567-labs/bge-base-en-v1.5-ft-quora-0.7",
+        # "567-labs/bge-base-en-v1.5-ft-quora-0.5",
+        # "567-labs/bge-base-en-v1.5-ft-quora-0.3",
+        # "567-labs/bge-base-en-v1.5-ft-quora-0.1",
+    ]
 
-    # for model_name, auc in zip(
-    #     models, benchmark_mteb_model.map(models, order_outputs=True)
-    # ):
-    #     res[model_name] = auc
+    for model_name, auc in zip(
+        models, benchmark_mteb_model.map(models, order_outputs=True)
+    ):
+        res[model_name] = auc
 
     values = [[model, auc] for model, auc in res.items()]
     values.sort(key=lambda x: x[1], reverse=True)
